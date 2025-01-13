91a7d0d37b41541fde68ef261e4afad0247feaf9
RyomenDev <aakashvatsh007@gmail.com>
2025-01-13 17:34:21 +0530

    oops

--- _48_oops.py
+++ _48_oops.py
@@ -1,10 +1,13 @@
 
+# object oriented programming
+
 # object - attribute; behavior
 
 # object; classes; encapsulation; abstraction; polymorphism
 
 # class- design
 # object- instance
+# methods -  to define behavious: function
 
 a = bin(11)
 # a=8
